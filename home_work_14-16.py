5597f786b55cbe3818e58ee56a221c93064b7c03
alexskvortsov <agskvortsov@gmail.com>
2017-04-21 19:03:14 +0300

    Tasks 01-10

--- home_work_14-16.py
+++ home_work_14-16.py
@@ -16,16 +16,28 @@
 # 15 Написать функцию, которая отвечает на вопрос, пересекаются ли две заданные окружности на плоскости.
     # Каждая окружность задается координатами центра и радиусом.
 
-# import math
-# def round_crossing_check(first_circle_coord_x, first_circle_coord_y, first_circle_radius, second_circle_coord_x, second_circle_coord_y, second_circle_radius):
-#     distance_between_centrs = math.sqrt((abs(first_circle_coord_x - second_circle_coord_x))**2 + (abs(first_circle_coord_y - second_circle_coord_y))**2)
-#     if distance_between_centrs <= ( first_circle_radius + second_circle_radius ):
-#         print("Circles are crossing")
-#     if distance_between_centrs > ( first_circle_radius + second_circle_radius ):
-#         print("Circles are not crossing")
-#     return distance_between_centrs
-#
-# x = round_crossing_check(5, -10, 11, 3, 2, 3)
+import math
+def round_crossing_check(first_circle_coord_x, first_circle_coord_y, first_circle_radius, second_circle_coord_x,
+                         second_circle_coord_y, second_circle_radius):
+
+    if (first_circle_coord_x - second_circle_coord_x) == 0 or (first_circle_coord_y - second_circle_coord_y) == 0:
+        if (first_circle_radius + second_circle_radius) >= (abs(first_circle_coord_x - second_circle_coord_x) +
+                                                                abs(first_circle_coord_y - second_circle_coord_y)):
+            print("Circles are crossing")
+        if (first_circle_radius + second_circle_radius) < (abs(first_circle_coord_x - second_circle_coord_x) +
+                                                               abs(first_circle_coord_y - second_circle_coord_y)):
+            print("Circles are not crossing")
+
+    if (first_circle_coord_x - second_circle_coord_x) != 0 and (first_circle_coord_y - second_circle_coord_y) != 0:
+        distance_between_centrs = math.sqrt((abs(first_circle_coord_x - second_circle_coord_x)) ** 2 + (
+                                    abs(first_circle_coord_y - second_circle_coord_y)) ** 2)
+        if distance_between_centrs <= ( first_circle_radius + second_circle_radius ):
+            print("Circles are crossing")
+        if distance_between_centrs > ( first_circle_radius + second_circle_radius ):
+            print("Circles are not crossing")
+
+
+x = round_crossing_check(1, 1, 1 , 3, 1, 1)
 
 
 # 16 Два поезда движутся на скорости V1 и V2 навстречу друг другу. Между ними 10 км. пути.
@@ -46,6 +58,4 @@
 #         print("Keep going whith current speed")
 #     if (v1/v2) <= (4/6):
 #         print("STOP! Train crash expected")
-
-# 17 Написать функцию решения квадратного уравнения.
 
